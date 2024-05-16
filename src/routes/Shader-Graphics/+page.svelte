fd2aae406502cebc32d9a027319487f956edbb9c
ringkun <ringkun12@gmail.com>
2024-05-16 01:14:00 -0600

    Some dependency issue. Hopefully works on vercel

--- src/routes/Shader-Graphics/+page.svelte
+++ src/routes/Shader-Graphics/+page.svelte
@@ -4,18 +4,22 @@
 <PageDefault> 
     
 </PageDefault>
-<iframe id="toggleBG" title = "funky backdrop" frameborder="0" src="https://www.shadertoy.com/embed/mldBRn?gui=false&t=10&paused=false&muted=false" allowfullscreen></iframe>
-<iframe id="toggleBG" title = "funky backdrop" frameborder="0" src="https://www.shadertoy.com/embed/XcdSWN?gui=false&t=10&paused=false&muted=false" allowfullscreen></iframe>
-<iframe id="toggleBG" title = "funky backdrop" frameborder="0" src="https://www.shadertoy.com/embed/lf3XDX?gui=false&t=10&paused=false&muted=false" allowfullscreen></iframe>
+<div class="BodyText">A showcase of computer graphics made using WebGL, hosted on Shadertoy.com. Unfortunately this is not a showcase for CSS, which scares me as of now.
+</div>
+<div class="BodyText">Please save me Gina.
+</div>
 
+<iframe id="toggleBG" title = "funky backdrop" frameborder="0" src="https://www.shadertoy.com/embed/mldBRn?gui=false&t=10&paused=false&muted=false" allowfullscreen></iframe>
+<iframe id="toggleBG" title = "Dotted Matrix" frameborder="0" src="https://www.shadertoy.com/embed/XcdSWN?gui=false&t=10&paused=false&muted=false" allowfullscreen></iframe>
+<iframe id="toggleBG" title = "Flamey" frameborder="0" src="https://www.shadertoy.com/embed/lf3XDX?gui=false&t=10&paused=false&muted=false" allowfullscreen></iframe>
 <style>
 
 
     iframe {
         position: relative; 
         width: 40%; 
-        height: 50%; 
-        top: 60px; 
+        height: 20%; 
+        top: 20px; 
         right: 0; 
         bottom: 0; 
         left: 0; 
