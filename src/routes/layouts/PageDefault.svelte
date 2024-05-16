0c44e169db49d0bd5c5052058448c5dc09e4b3cf
ringkun <ringkun12@gmail.com>
2024-05-15 22:50:41 -0600

    Ugly iframe background removed. We now got a new "Shadergraphics" page baybeee

--- src/routes/layouts/PageDefault.svelte
+++ src/routes/layouts/PageDefault.svelte
@@ -1,3 +1,9 @@
+<!-- 
+    mldBRn
+    XcdSWN
+    Mf3XDX
+ -->
+
 <TopBar>
 
 </TopBar>
@@ -8,41 +14,30 @@
 
 <AffiliateLink></AffiliateLink>
 
-<iframe id="toggleBG" title = "funky backdrop" frameborder="0" src="https://www.shadertoy.com/embed/mldBRn?gui=false&t=10&paused=false&muted=false" allowfullscreen></iframe>
 
 <script>
+    
     import './global.scss'
     import TopBar from "./TopBar.svelte";
     import AffiliateLink from "./AffiliateLink.svelte";
-    function toggleBG() {
-        console.log("Working");
-        let tBG = document.getElementById("toggleBG");
-        if (tBG === null){
-            alert("Doesn't exist");
-        }
-        else{        
-            if (tBG.style.display === "none") {
-                tBG.style.display = "block";
-            } else {
-                tBG.style.display = "none";
-            };
-        }
+    // function toggleBG() {
+    //     console.log("Working");
+    //     let tBG = document.getElementById("toggleBG");
+    //     if (tBG === null){
+    //         alert("Doesn't exist");
+    //     }
+    //     else{        
+    //         if (tBG.style.display === "none") {
+    //             tBG.style.display = "block";
+    //         } else {
+    //             tBG.style.display = "none";
+    //         };
+    //     }
+    // };
 
-    };
+    
 </script>
 <style>
 
-
-    iframe {
-        position: fixed; 
-        width: 100%; 
-        height: 100%; 
-        top: 60px; 
-        right: 0; 
-        bottom: 0; 
-        left: 0; 
-        z-index: -1; 
-        pointer-events: none; 
-    }    
 
 </style>
