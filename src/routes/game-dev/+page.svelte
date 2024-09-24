000d09ae220f344ac5aebcc54ee305b9d7a11e91
ringkun <ringkun12@gmail.com>
2024-09-24 00:30:38 -0600

    Added text content on the game dev page

--- src/routes/game-dev/+page.svelte
+++ src/routes/game-dev/+page.svelte
@@ -5,7 +5,15 @@
 
 <PageDefault> 
 </PageDefault>
-
+<div class="BodyText">
+  <p>
+  
+    When I started off programming, like many other, I did some light game development. Eventually at some point as I became more ambitious with my development, I refined my workflow which lead me to develop my own plugins, addons, and macros to expediate my development process. For example I made my own custom 3D skeleton rigging utility that effectively reduce my work time by half. I always found the process rewarding. Even further more when I shared my tools for others to use. I felt fulfillment in being able to help others like this. That's the reason I pursued computer science as a major, since I believe that was the pathway into how I can help others in the world, a grand mission for me. 
+    
+    I think this company will allow me to gain experience in the real world to pursue this mission. 
+    
+  </p>
+</div>
 
 
 
