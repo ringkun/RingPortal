1105f33ecc26af4b03261171221a1bd43d5807ac
ringkun <ringkun12@gmail.com>
2025-04-10 05:05:13 -0600

    No clue what's going on, hopefully it works

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.0d5155f5.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js","_app/immutable/chunks/PageDefault.b5efc0fe.js","_app/immutable/chunks/paths.bd4f7188.js"];
+export const imports = ["_app/immutable/nodes/1.e0212066.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js","_app/immutable/chunks/PageDefault.2f53ed78.js","_app/immutable/chunks/paths.e4fc72f1.js"];
 export const stylesheets = ["_app/immutable/assets/PageDefault.0d7bd511.css"];
 export const fonts = [];
