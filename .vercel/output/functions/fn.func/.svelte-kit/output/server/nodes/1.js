72db802c407976896260b71b6fe28af456957a3d
ringkun <ringkun12@gmail.com>
2025-04-10 04:43:40 -0600

    Got overzealous, made it v3 instead of v4. Hope this works

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.06d4dad5.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js","_app/immutable/chunks/PageDefault.869fafc9.js","_app/immutable/chunks/paths.d30122e6.js"];
+export const imports = ["_app/immutable/nodes/1.da00d677.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js","_app/immutable/chunks/PageDefault.5a7c42ac.js","_app/immutable/chunks/paths.1b98b4db.js"];
 export const stylesheets = ["_app/immutable/assets/PageDefault.0d7bd511.css"];
 export const fonts = [];
