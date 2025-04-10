57126e221ee7fcd968d689293cb2b1d6b9cc636e
ringkun <ringkun12@gmail.com>
2025-04-10 04:41:56 -0600

    Changed the version number of upload-pages-artifact

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".nojekyll","Animation/Sunshine.mp4","favicon.png","images/DurandalView.png","images/LaevatinnView.png","images/MimicryView.png","SVG/WJX.svg"]),
 	mimeTypes: {".mp4":"video/mp4",".png":"image/png",".svg":"image/svg+xml"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.1ff9119c.js","app":"_app/immutable/entry/app.4d11792b.js","imports":["_app/immutable/entry/start.1ff9119c.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/paths.3be87d9c.js","_app/immutable/entry/app.4d11792b.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.6add48cc.js","app":"_app/immutable/entry/app.a4966a38.js","imports":["_app/immutable/entry/start.6add48cc.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/paths.d30122e6.js","_app/immutable/entry/app.a4966a38.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
