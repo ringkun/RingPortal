0b00d06107cb3d4e64173b7bfc789703e41479f4
ringkun <ringkun12@gmail.com>
2025-04-10 04:54:57 -0600

    Whoops forgot to npm run build

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.da00d677.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js","_app/immutable/chunks/PageDefault.5a7c42ac.js","_app/immutable/chunks/paths.1b98b4db.js"];
+export const imports = ["_app/immutable/nodes/1.0d5155f5.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js","_app/immutable/chunks/PageDefault.b5efc0fe.js","_app/immutable/chunks/paths.bd4f7188.js"];
 export const stylesheets = ["_app/immutable/assets/PageDefault.0d7bd511.css"];
 export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".nojekyll","Animation/Sunshine.mp4","favicon.png","images/DurandalView.png","images/LaevatinnView.png","images/MimicryView.png","SVG/WJX.svg"]),
 	mimeTypes: {".mp4":"video/mp4",".png":"image/png",".svg":"image/svg+xml"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.baa0bb7b.js","app":"_app/immutable/entry/app.2d745ba3.js","imports":["_app/immutable/entry/start.baa0bb7b.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/paths.1b98b4db.js","_app/immutable/entry/app.2d745ba3.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.5f6d0bf6.js","app":"_app/immutable/entry/app.165625e0.js","imports":["_app/immutable/entry/start.5f6d0bf6.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/paths.bd4f7188.js","_app/immutable/entry/app.165625e0.js","_app/immutable/chunks/scheduler.2aecb5fb.js","_app/immutable/chunks/index.6cc61874.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
